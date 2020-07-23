db7444ab7ad772dca0e489806af011b164416ba5
jay <johnjohnsonokah@gmail.com>
2020-07-23 15:13:27 +0100

    Create catalog template

--- shop/views.py
+++ shop/views.py
@@ -18,7 +18,7 @@ def product_list(request, category_slug=None):
         products = products.filter(category=category)
     return render(
         request,
-        "/shop/product/list.html",
+        "shop/product/list.html",
         {"category": category, "categories": categories, "products": products},
     )
 
@@ -27,4 +27,4 @@ def product_detail(request, id, slug):
     """Retrieve and display a single product
     """
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, "/shop/product/detail.html", {"product": product})
+    return render(request, "shop/product/detail.html", {"product": product})
