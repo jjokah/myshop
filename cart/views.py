c5a6eb1f6b7ed94c964549d5735a003155854d3c
jay <johnjohnsonokah@gmail.com>
2020-07-30 10:51:19 +0100

    Update product quantities in the cart.

--- cart/views.py
+++ cart/views.py
@@ -44,4 +44,8 @@ def cart_detail(request):
     Display the products in cart
     """
     cart = Cart(request)
+    for item in cart:
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={"quantity": item["quantity"], "override": True}
+        )
     return render(request, "cart/detail.html", {"cart": cart})
