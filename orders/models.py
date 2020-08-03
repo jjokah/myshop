fe4db4ce82dbc91a398305c8261a38af1f76a36c
jay <johnjohnsonokah@gmail.com>
2020-08-03 02:43:47 +0100

    Create order models

---
+++ orders/models.py
@@ -0,0 +1,55 @@
+"""
+Manage orders in the shop
+"""
+from django.db import models
+from shop.models import Product
+
+
+class Order(models.Model):
+    """
+    Stores infomation about the customer making order
+    """
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+    def get_total_cost(self):
+        """
+        Returns the total cost of items.
+        """
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    """
+    Stores the product, quantity, and price paid for each item
+    """
+
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="order_items", on_delete=models.CASCADE
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        """
+        Returns the cost of each item
+        """
+        return self.price * self.quantity
