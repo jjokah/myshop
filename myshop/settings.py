21b5aa811f938559ddcddedbdbb9dffefb6bea19
jay <johnjohnsonokah@gmail.com>
2020-09-04 14:56:31 +0100

    Add python-decouple package and separate secret key

--- myshop/settings.py
+++ myshop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 import django_heroku
+from decouple import config
 
 env = environ.Env()
 
@@ -24,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "q0-hg=j68b*04-9sx2p1+65+&3o4f-tl14ongf(4*!(%u++)-n"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
