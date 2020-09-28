6f4110f772af8e00e1f95110c637f36e8ec1c9b1
jay <johnjohnsonokah@gmail.com>
2020-09-28 22:21:27 +0100

    Use CloudinaryField for image in model field

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,8 @@ This module holds models of every object in the shop.
 from django.db import models
 from django.urls import reverse
 
+from cloudinary.models import CloudinaryField
+
 
 class Category(models.Model):
     """ The category model each product is classified into. """
@@ -22,8 +24,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        """ Retrieve url for list of product in a given category
-        """
+        """Retrieve url for list of product in a given category"""
         return reverse("shop:product_list_by_category", args=[self.slug])
 
 
@@ -35,7 +36,7 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    image = CloudinaryField("product")
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -50,6 +51,5 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        """Retrieves url for a given product
-        """
+        """Retrieves url for a given product"""
         return reverse("shop:product_detail", args=[self.id, self.slug])
