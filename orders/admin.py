e3efb0e873a6c458646bad37ce56b7e11597c9ef
jay <johnjohnsonokah@gmail.com>
2020-11-28 17:19:26 +0100

    Extend the admin site with custom views

--- orders/admin.py
+++ orders/admin.py
@@ -6,6 +6,8 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -48,6 +50,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ["product"]
 
 
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """
@@ -65,6 +72,7 @@ class OrderAdmin(admin.ModelAdmin):
         "paid",
         "created",
         "updated",
+        order_detail,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
