27c4af85b94f0949f83f3970eb9b1b43d2a1fd45
jay <johnjohnsonokah@gmail.com>
2021-01-16 18:39:44 +0100

    URL patterns for internationalization

--- myshop/settings.py
+++ myshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "cloudinary_storage",
     "cloudinary",
+    "rosetta",
     "shop.apps.ShopConfig",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
--- orders/urls.py
+++ orders/urls.py
@@ -4,12 +4,14 @@ Urls related to orders placed on the shop
 
 
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
 from . import views
 
 app_name = "orders"
 
 urlpatterns = [
-    path("create/", views.order_create, name="order_create"),
+    path(_("create/"), views.order_create, name="order_create"),
     path(
         "admin/order/<int:order_id>/",
         views.admin_order_detail,
